9d0b5971352132f70ebe4bcb8aec3d60d28ae2e3
prashant <pipaliyaprashant602@gamil.com>
2022-02-28 11:22:38 +0530

    calculator-commit

--- src/App.js
+++ src/App.js
@@ -1,43 +1,53 @@
 import React, { Component } from "react";
 
-class App extends Component{
+class App extends Component {
 
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      data : ""
+      data: "",
+      array: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     }
   }
 
   clickButton = (e) => {
-    this.setState({ data : this.state.data + e.target.value })
+    this.shuffleArray(this.state.array);
+    this.setState({ data: this.state.data + e.target.value })
     // + indicate concatination of two string
   }
 
   clearValue = () => {
-    this.setState({ data : "" })
+    this.setState({ data: "" })
   }
 
   backSpace = () => {
-    this.setState({ data : this.state.data.slice(0, -1) })
+    this.setState({ data: this.state.data.slice(0, -1) })
     // -1 indicte this.state.data.length -1
+  }
+
+  shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    this.setState({ array: array })
   }
 
   calculateResult = () => {
 
     try {
-      this.setState({ data : eval(this.state.data).toString() })
+      this.setState({ data: eval(this.state.data).toString() })
     }
     catch (error) {
-      this.setState({ data : "error" })
+      this.setState({ data: "error" })
       console.log(error);
     }
   }
 
-  render(){
-    return(
+  render() {
+    return (
       <>
-          
+
         <h1 className="text-center mt-3" >The Calculator</h1>
 
         <div className="calc_main">
@@ -47,24 +57,24 @@ class App extends Component{
           <input type="button" value="CE" onClick={this.backSpace} />
           <input type="button" value=")" onClick={this.clickButton} />
           <input type="button" value="C" onClick={this.clearValue} />
-          <input type="button" value="9" onClick={this.clickButton} />
-          <input type="button" value="8" onClick={this.clickButton} />
-          <input type="button" value="7" onClick={this.clickButton} />
+          <input type="button" value={this.state.array[9]} onClick={this.clickButton} />
+          <input type="button" value={this.state.array[8]} onClick={this.clickButton} />
+          <input type="button" value={this.state.array[7]} onClick={this.clickButton} />
           <input type="button" value="+" onClick={this.clickButton} />
-          <input type="button" value="6" onClick={this.clickButton} />
-          <input type="button" value="5" onClick={this.clickButton} />
-          <input type="button" value="4" onClick={this.clickButton} />
+          <input type="button" value={this.state.array[6]} onClick={this.clickButton} />
+          <input type="button" value={this.state.array[5]} onClick={this.clickButton} />
+          <input type="button" value={this.state.array[4]} onClick={this.clickButton} />
           <input type="button" value="-" onClick={this.clickButton} />
-          <input type="button" value="3" onClick={this.clickButton} />
-          <input type="button" value="2" onClick={this.clickButton} />
-          <input type="button" value="1" onClick={this.clickButton} />
+          <input type="button" value={this.state.array[3]} onClick={this.clickButton} />
+          <input type="button" value={this.state.array[2]} onClick={this.clickButton} />
+          <input type="button" value={this.state.array[1]} onClick={this.clickButton} />
           <input type="button" value="*" onClick={this.clickButton} />
           <input type="button" value="." onClick={this.clickButton} />
-          <input type="button" value="0" onClick={this.clickButton} />
+          <input type="button" value={this.state.array[0]} onClick={this.clickButton} />
           <input type="button" value="=" onClick={this.calculateResult} />
           <input type="button" value="/" onClick={this.clickButton} />
         </div>
-  
+
       </>
     )
   }
